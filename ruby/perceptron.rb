da0851a7160165bbd683713bf82156f86d21a9e0
Manuel Hutter <manuel@hutter.io>
2016-02-18 15:49:11 +0100

    Refactored the Ruby variant
    - implemented the code object oriented
    - moved core extensions into refinements
    - colored the output :-)
    - added some minimalist documentation

--- ruby/perceptron.rb
+++ ruby/perceptron.rb
@@ -1,57 +1,94 @@
 # A Perceptron in Ruby
 
 # Some helper methods
-class Numeric
-  def heaviside
-    self < 0 ? 0 : 1
+module Refinements
+  refine Numeric do
+    def heaviside
+      self < 0 ? 0 : 1
+    end
   end
-end
 
-class Array
-  def dot(other)
-    self.zip(other).map { |x,y| x*y }.inject(:+)
+  refine Array do
+    def dot(other)
+      zip(other).map { |x, y| x * y }.inject(:+)
+    end
   end
 end
 
 # definitions for output
-outmap = {
+OUTMAP = {
   -1 => '-',
-   0 => '.',
-   1 => '+'
-}
+  0 => '.',
+  1 => '+'
+}.freeze
 
-training_data = [
+TRAINING_DATA = [
   [[0, 0, 1], 0],
   [[0, 1, 1], 1],
   [[1, 0, 1], 1],
-  [[1, 1, 1], 1],
-]
+  [[1, 1, 1], 1]
+].freeze
 
-weight = Array.new(3).map! { rand }
-errors = []
-eta = 0.2
-n = 100
+N = 100
 
-puts "initial weight: #{weight}"
+# A simple perceptron classifier.
+class Perceptron
+  using Refinements
+  ETA = 0.2
 
-n.times do
-  data, expected = training_data.sample
-  result = data.dot(weight)
-  error = expected - result.heaviside
-  errors << error
+  attr_accessor :weight, :errors
 
-  for i in 0...weight.size
-    weight[i] += eta * error * data[i]
+  def initialize(size)
+    @size = size
+    @weight = Array.new(@size).map! { rand }
+    @data = Array.new(@size)
+    @errors = []
+  end
+
+  # Update the Perceptron with new data to work with.
+  def update(data)
+    @data = data
+  end
+
+  # calculate the normalized (heaviside) result based on the current ``@data`
+  def result
+    raw_result.heaviside
+  end
+
+  # calculate the result based on the current `@data`
+  def raw_result
+    @data.dot(weight)
+  end
+
+  # update the `@weight`s based on the current `@data`
+  def train(error)
+    @errors << error
+
+    (0...@size).each do |i|
+      @weight[i] += ETA * error * @data[i]
+    end
   end
 end
 
-training_data.each do |data,expected|
-  result = data.dot(weight)
-  correct = result.heaviside == expected ? '✓' : '✗'
-  puts "%s: % .17f -> %s %s" % [data[0,2], result, result.heaviside, correct]
+p = Perceptron.new(3)
+
+puts "initial weight: #{p.weight}"
+
+N.times do
+  data, expected = TRAINING_DATA.sample
+  p.update data
+  error = expected - p.result
+  p.train(error)
 end
 
-puts "final weight: #{weight}"
+TRAINING_DATA.each do |data, expected|
+  p.update data
+  ok, nok = "\e[0;32m✓\e[m\e[m", "\e[0;31m✗\e[m\e[m"
+  correct = p.result == expected ? ok : nok
+  puts format('%s: % .7f -> %s %s', data[0, 2], p.raw_result, p.result, correct)
+end
+
+puts "final weight: #{p.weight}"
 
 # display the progress
-puts errors.map { |e| outmap[e] }.join
+puts p.errors.map { |e| OUTMAP[e] }.join
