dd4cc142dd80f79da6676dce6cc77df1dde7bcb7
Manuel Hutter <manuel@hutter.io>
2016-02-19 08:58:19 +0100

    simplified the code

--- ruby/perceptron.rb
+++ ruby/perceptron.rb
@@ -15,80 +15,57 @@ module Refinements
   end
 end
 
+using Refinements
+
 # definitions for output
 OUTMAP = {
   -1 => '-',
   0 => '.',
   1 => '+'
-}.freeze
+}
+OK  = "\e[0;32m✓\e[m\e[m"
+NOK = "\e[0;31m✗\e[m\e[m"
 
+# constant data
 TRAINING_DATA = [
   [[0, 0, 1], 0],
   [[0, 1, 1], 1],
   [[1, 0, 1], 1],
   [[1, 1, 1], 1]
-].freeze
-
+]
+ETA = 0.2
 N = 100
 
-# A simple perceptron classifier.
-class Perceptron
-  using Refinements
-  ETA = 0.2
+# runtime data
+weight = Array.new(3).map! { rand }
+errors = []
 
-  attr_accessor :weight, :errors
-
-  def initialize(size)
-    @size = size
-    @weight = Array.new(@size).map! { rand }
-    @data = Array.new(@size)
-    @errors = []
-  end
-
-  # Update the Perceptron with new data to work with.
-  def update(data)
-    @data = data
-  end
-
-  # calculate the normalized (heaviside) result based on the current ``@data`
-  def result
-    raw_result.heaviside
-  end
-
-  # calculate the result based on the current `@data`
-  def raw_result
-    @data.dot(weight)
-  end
-
-  # update the `@weight`s based on the current `@data`
-  def train(error)
-    @errors << error
-
-    (0...@size).each do |i|
-      @weight[i] += ETA * error * @data[i]
-    end
-  end
-end
-
-p = Perceptron.new(3)
-
-puts "initial weight: #{p.weight}"
+puts "initial weight: #{weight}"
 
 N.times do
   data, expected = TRAINING_DATA.sample
-  p.update data
-  error = expected - p.result
-  p.train(error)
+  result = data.dot(weight)
+  error = expected - result.heaviside
+  errors << error
+
+  (0...weight.size).each do |i|
+    weight[i] += ETA * error * data[i]
+  end
 end
 
+# output for control
 TRAINING_DATA.each do |data, expected|
-  p.update data
-  ok, nok = "\e[0;32m✓\e[m\e[m", "\e[0;31m✗\e[m\e[m"
-  correct = p.result == expected ? ok : nok
-  puts format('%s: % .7f -> %s %s', data[0, 2], p.raw_result, p.result, correct)
+  result = data.dot(weight)
+
+  correct = result.heaviside == expected ? OK : NOK
+  puts format('%s: % .7f -> %s %s',
+              data[0, 2],
+              result,
+              result.heaviside,
+              correct)
 end
 
-puts "final weight: #{p.weight}"
+puts "final weight: #{weight}"
 
 # display the progress
-puts p.errors.map { |e| OUTMAP[e] }.join
+puts errors.map { |e| OUTMAP[e] }.join
